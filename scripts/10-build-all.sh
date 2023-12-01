1321396f805e0d26db70acee2ff25fd80fb84cb5
Bruce W. Herr II <bherr@indiana.edu>
2023-12-01 09:56:07 -0500

    Build non-collections before collections to optimize a bit

--- scripts/10-build-all.sh
+++ scripts/10-build-all.sh
@@ -3,11 +3,23 @@ source constants.sh
 shopt -s extglob
 set -ev
 
-BUILD_OPTS="--clean"
+BUILD_OPTS=""
+CLEAN_DOs="--clean"
 PROCESSOR_OPTS="--exclude-bad-values"
-DIGITAL_OBJECTS=`do-processor list`
+
+echo "Building Digital Objects..."
+echo
 
 set +e # Ignore errors
-for obj in $DIGITAL_OBJECTS; do
-  do-processor $PROCESSOR_OPTS build $BUILD_OPTS $obj
+for obj in $(do-processor list | grep -v '^collection'); do
+  do-processor $PROCESSOR_OPTS build $BUILD_OPTS $CLEAN_DOs $obj
+done
+
+echo "Building Collections..."
+echo
+
+for obj in $(do-processor list | grep '^collection'); do
+  do-processor $PROCESSOR_OPTS normalize $BUILD_OPTS $obj
+  do-processor $PROCESSOR_OPTS enrich $BUILD_OPTS $obj
+  do-processor $PROCESSOR_OPTS deploy $BUILD_OPTS $obj
 done
