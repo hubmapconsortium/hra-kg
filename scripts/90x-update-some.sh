d092b82c613e9c1665bdcd9de7f10fe78074c83b
Bruce W. Herr II <bherr@indiana.edu>
2023-12-15 10:46:03 -0500

    Update ccf.owl

--- scripts/90x-update-some.sh
+++ scripts/90x-update-some.sh
@@ -3,12 +3,13 @@ source constants.sh
 shopt -s extglob
 set -ev
 
-BUILD_OPTS="--clean --update-db"
+DEFAULT_BUILD_OPTS="--clean --update-db"
+BUILD_OPTS=${BUILD_OPTS:=${DEFAULT_BUILD_OPTS}}
 PROCESSOR_OPTS="--exclude-bad-values"
 DIGITAL_OBJECTS=$@
 
 for obj in $DIGITAL_OBJECTS; do
-  do-processor $PROCESSOR_OPTS build $BUILD_OPTS $obj
+  do-processor $PROCESSOR_OPTS build $CLEAN $BUILD_OPTS $obj
 
   if [ "$(echo $obj | cut -d '/' -f 3)" != "draft" ]; then
     rsync -r dist/$obj/ $(dirname dist/$obj)/latest/
