771ddf19f59e8516373210be9961e0f6998ff9ab
Bruce W. Herr II <bherr@indiana.edu>
2024-06-10 10:05:22 -0400

    Added code to generate all compiled asct+b json files for all releases

---
+++ scripts/99x-gen-asctb-json.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source ../hra-do-processor/.venv/bin/activate
+set -ev
+
+export COLLECTIONS=$(do-processor list | grep '^collection/hra/' | sort);
+OUT=scratch/hra-asctb-json-releases
+
+mkdir -p $OUT
+rm -f $OUT/*
+
+for COLL in $COLLECTIONS; do
+  VERSION=$(echo $COLL | cut -d '/' -f 3);
+  do-processor gen-asct-b-collection-json $COLL --output $OUT/hra-asctb-all.$VERSION.json
+done
+
+aws s3 sync ${OUT}/ s3://cdn-humanatlas-io/hra-asctb-json-releases/
