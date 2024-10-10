fe2c1f7869a846c3984416a9cfbe0b2f300e572e
Bruce W. Herr II <bherr@iu.edu>
2024-10-10 13:33:58 -0400

    Add collection script for removing individuals

---
+++ scripts/90x-update-collections-no-indviduals.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+source constants.sh
+shopt -s extglob
+set -ev
+
+BUILD_OPTS=""
+DEPLOY_OPTS="--update-db"
+PROCESSOR_OPTS="--exclude-bad-values --remove-individuals"
+DIGITAL_OBJECTS=$@
+
+for obj in $DIGITAL_OBJECTS; do
+  do-processor $PROCESSOR_OPTS normalize $BUILD_OPTS $obj
+  do-processor $PROCESSOR_OPTS enrich $BUILD_OPTS $obj
+  do-processor $PROCESSOR_OPTS deploy $DEPLOY_OPTS $BUILD_OPTS $obj
+
+  if [ "$(echo $obj | cut -d '/' -f 3)" != "draft" ]; then
+    rsync -r dist/$obj/ $(dirname dist/$obj)/latest/
+  fi
+done
