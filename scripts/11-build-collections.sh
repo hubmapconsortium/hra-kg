e7f6c22b71a0e546a032aa02aa05940b92691950
Bruce W. Herr II <bherr@iu.edu>
2025-06-04 09:47:30 -0400

    Separate out digital object and collection building for ease of use

--- scripts/11-build-collections.sh
+++ scripts/11-build-collections.sh
@@ -6,28 +6,6 @@ set -ev
 BUILD_OPTS=""
 CLEAN_DOs="--clean"
 PROCESSOR_OPTS="--exclude-bad-values"
-SKIP_BUILT_DOs="true"
-
-echo "Building Digital Objects..."
-echo
-
-rm -f jobs.txt
-queue_job() {
-  echo "$1" >> jobs.txt
-}
-wait_for_empty_queue() {
-  node ./src/parallel-jobs.js jobs.txt
-  rm -f jobs.txt
-}
-
-for obj in $(do-processor list | grep -v '^collection' | shuf); do
-  if [ "${SKIP_BUILT_DOs}" = "false" ] || [ ! -e dist/${obj}/graph.ttl ]; then
-    mkdir -p dist/${obj}
-    queue_job "./src/run-do-processor.sh $PROCESSOR_OPTS build $BUILD_OPTS $CLEAN_DOs $obj"
-  fi
-done
-
-wait_for_empty_queue
 
 echo "Building Collections..."
 echo
