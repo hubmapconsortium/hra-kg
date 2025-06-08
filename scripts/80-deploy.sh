6e20a05b0fae603917774243c7b69bd1914a88de
Bruce W. Herr II <bherr@iu.edu>
2025-06-07 22:01:27 -0400

    Add case for staging

--- scripts/80-deploy.sh
+++ scripts/80-deploy.sh
@@ -12,3 +12,7 @@ if [ "${DEFAULT_CDN_IRI}" = "https://cdn.humanatlas.io/digital-objects/" ]; then
   rsync -ri $CLEAN --checksum ./dist/ ./staging/
   aws s3 sync $CLEAN ./staging/ s3://cdn-humanatlas-io/digital-objects/
 fi
+
+if [ "${DEFAULT_CDN_IRI}" = "https://cdn.humanatlas.io/hra-kg--staging/" ]; then
+  aws s3 sync $CLEAN ./dist/ s3://cdn-humanatlas-io/hra-kg--staging/
+fi
