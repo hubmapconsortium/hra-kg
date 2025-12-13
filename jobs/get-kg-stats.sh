2005f928b44411c19a5bf81a19ad5646ef61bb2b
Bruce W. Herr II <bherr@iu.edu>
2025-12-13 09:00:20 -0500

    Update KG stats computing

--- jobs/get-kg-stats.sh
+++ jobs/get-kg-stats.sh
@@ -3,29 +3,29 @@ source constants.sh
 shopt -s extglob
 set -ev
 
-JNL='dist/ALL-blazegraph.jnl'
-rm -f $JNL
+JNL=scratch/ALL-nodes.jnl
+ALL_NODES=scratch/ALL-nodes.tsv
+rm -f $ALL_NODES
+for NT in `find dist -name "*.nt" | shuf`; do
+  rm -f $JNL
+  blazegraph-runner --journal=$JNL load $NT
+  blazegraph-runner --journal=$JNL select src/nodes.rq scratch/ALL-nodes-1.tsv
+  tail -n +2 scratch/ALL-nodes-1.tsv >> $ALL_NODES
+done
 
-do-processor create-db --include-all-versions --journal $JNL
-
-blazegraph-runner --journal=$JNL select src/nodes.rq dist/ALL-nodes.tsv
-blazegraph-runner --journal=$JNL select src/edge-count.rq dist/edge-count.tsv
-
-NODES=`tail -n +2 dist/ALL-nodes.tsv | sort -u -S 75% | wc -l`
+NODES=`tail -n +2 $ALL_NODES | sort -u -S 75% | wc -l`
 echo nodes $NODES
+rm -f scratch/ALL-nodes-1.tsv $ALL_NODES $JNL
 
-EDGES=`tail -1 dist/edge-count.tsv | cut -d'"' -f 2`
+EDGES=`find dist -name "*.nt" -exec cat {} \; | wc -l`
 echo edges $EDGES
 
-EDGES2=`find dist -name "*.nt" -exec cat {} \; | wc -l`
-echo edges2 $EDGES2
-
 SIZE=`du -s -BM dist | perl -pe 's/[^0-9]+//g'`
-echo size $SIZE
+echo size $SIZE MB, `du -hs dist`
 
 echo "label,count,unit" > dist/high-level-stats.csv
 echo "Nodes in the HRA Knowledge Graph,${NODES}," >> dist/high-level-stats.csv
 echo "Edges in the HRA Knowledge Graph,${EDGES}," >> dist/high-level-stats.csv
 echo "Size of the HRA Knowledge Graph,${SIZE},MB" >> dist/high-level-stats.csv
 
-rm -f dist/ALL-nodes.tsv dist/edge-count.tsv
+rm -f $ALL_NODES
